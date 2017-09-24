bedfdd51b0c4c26cdb7711f372456df54a7e373d
Lukas Zeller <luz@plan44.ch>
2017-09-24 16:52:42 +0200

    Initial Commit

---
+++ LocalSites/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  LocalSites
+//
+//  Created by Lukas Zeller on 24.09.17.
+//  Copyright © 2017 plan44.ch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
